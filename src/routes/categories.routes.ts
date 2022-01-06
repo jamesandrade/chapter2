8ee898f5e0707cef80e2381594ecb9c9c1adae4e
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-06 17:09:31 -0300

    Add SOLID concepts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,21 @@
 import { Router, Request, Response } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
-  const { name, description } = req.body;
-
-  const categoryAlredyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlredyExists) {
-    return res.status(400).json({ error: "Category Already Exists!" }).send();
-  }
-  categoriesRepository.create({ name, description });
-  return res.status(201).send();
+    const { name, description } = req.body;
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository
+    );
+    createCategoryService.execute({ name, description });
+    return res.status(201).send();
 });
 categoriesRoutes.get("/", (req: Request, res: Response) => {
-  const listAll = categoriesRepository.list();
-  return res.status(201).json(listAll).send();
+    const listAll = categoriesRepository.list();
+    return res.status(201).json(listAll).send();
 });
 export { categoriesRoutes };
