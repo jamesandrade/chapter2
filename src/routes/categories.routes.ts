b74b69f340ba6092c3420a852e371d07d3e755f9
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-12 19:05:19 -0300

    add tsyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,22 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+// import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
 const upload = multer({
     dest: "./tmp",
 });
-categoriesRoutes.post("/", (req: Request, res: Response) => {
-    return createCategoryController.handle(req, res);
-});
-categoriesRoutes.get("/", (req: Request, res: Response) => {
-    return listCategoriesController.handle(req, res);
-});
+
+const createCategoryController = new CreateCategoryController();
+categoriesRoutes.post("/", createCategoryController.handle);
+
+// categoriesRoutes.get("/", (req: Request, res: Response) => {
+//   return listCategoriesController.handle(req, res);
+// });
 
 categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
     return importCategoryController.handle(req, res);
