8fbbb9adb72f1c3f6074e61da4c6d8e5fc2edcd4
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-21 16:47:55 -0300

    init test with jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,14 @@
+describe("Criar categoria", () => {
+    it("test if 2 + 2 = 4", () => {
+        const soma = 2 + 2;
+        const result = 4;
+        expect(soma).toBe(result);
+    });
+
+    it("test if 2 + 2 not 5", () => {
+        const soma = 2 + 2;
+        const resultado = 5;
+
+        expect(soma).not.toBe(resultado);
+    });
+});
