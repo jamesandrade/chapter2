d81f9e1d6cc2098a6a16be6ededb954eae4a9f18
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-22 12:48:22 -0300

    Test in Create Category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,14 +1,41 @@
-describe("Criar categoria", () => {
-    it("test if 2 + 2 = 4", () => {
-        const soma = 2 + 2;
-        const result = 4;
-        expect(soma).toBe(result);
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+describe("Create Category", () => {
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory
+        );
+    });
+    it("should be able to create a new category", async () => {
+        const category = {
+            name: "test",
+            description: "test",
+        };
+        await createCategoryUseCase.execute({
+            name: "test",
+            description: "test",
+        });
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            category.name
+        );
+        expect(categoryCreated).toHaveProperty("id");
     });
 
-    it("test if 2 + 2 not 5", () => {
-        const soma = 2 + 2;
-        const resultado = 5;
-
-        expect(soma).not.toBe(resultado);
+    it("should not be able to create a new category with name exists", async () => {
+        expect(async () => {
+            await createCategoryUseCase.execute({
+                name: "test",
+                description: "test",
+            });
+            await createCategoryUseCase.execute({
+                name: "test",
+                description: "test",
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
