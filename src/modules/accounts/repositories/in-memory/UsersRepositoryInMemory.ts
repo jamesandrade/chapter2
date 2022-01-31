45c8d92c91c665a61667c6395413900b36dcc20b
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-31 18:07:07 -0300

    add in-memory usersrepository

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { User } from "../../entities/User";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+    users: User[] = [];
+    async create({
+        driver_license,
+        email,
+        name,
+        password,
+    }: ICreateUserDTO): Promise<void> {
+        const user = new User();
+        Object.assign(user, {
+            driver_license,
+            email,
+            name,
+            password,
+        });
+        this.users.push(user);
+    }
+    async findByEmail(email: string): Promise<User> {
+        return this.users.find((user) => user.email === email);
+    }
+    async findById(id: string): Promise<User> {
+        return this.users.find((user) => user.id === id);
+    }
+    async updateUserAvatar(id: string, avatar: string): Promise<void> {
+        const findIndex = this.users.findIndex((ind) => ind.id === id);
+        this.users[findIndex].avatar = avatar;
+    }
+}
+
+export { UsersRepositoryInMemory };
