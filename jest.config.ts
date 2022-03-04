7a6fbba1b33e0a23f6f115f4024315e5918345cc
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-03-04 16:49:51 -0300

    jest imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+
+import { compilerOptions } from "./tsconfig.json";
 /*
 
  * For a detailed explanation regarding each configuration property and type check, visit:
@@ -121,7 +125,10 @@ export default {
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
 
-    // moduleNameMapper: {},
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        // eslint-disable-next-line prettier/prettier
+        prefix: "<rootDir>/src/"
+    }),
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 
